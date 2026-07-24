fe5f7dba9e1a9b9bb94de839d40d39024f590d5a
agillet0851 <a.gillet08000@gmail.com>
2026-07-24 15:28:51 +0200

    Delete success.js

--- success.js
+++
@@ -1,8 +0,0 @@
-export default function Success() {
-  return (
-    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
-      <h1>🎉 Merci pour votre commande !</h1>
-      <p>Votre cadre est en cours de préparation dans notre atelier. Vous recevrez un e-mail avec votre numéro de suivi dès l'expédition.</p>
-    </div>
-  );
-}
